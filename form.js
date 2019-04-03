d05c565ec3bb66571d2b73d01c42fc3cc903216d
Gabriella Gintare Rozenaite <34273311+Gabriella-roze@users.noreply.github.com>
2019-04-03 19:48:17 +0200

    add functionality on first section in the form

--- form.js
+++ form.js
@@ -12,6 +12,9 @@ const btnShipping = document.getElementById("btn__shipping");
 const btnPersonal = document.getElementById("btn__personal");
 const btnPayment = document.getElementById("btn__payment");
 
+const btnFinalConfirm = document.getElementById("finalConfirm");
+
+
 // Inputs
 const inputQtySwap = document.getElementById("inputQtySwap");
 const inputQtyBattery = document.getElementById("inputQtyBattery");
@@ -27,6 +30,18 @@ const inputQtyBattery = document.getElementById("inputQtyBattery");
 // const inputQtySwap = document.getElementById("inputQtySwap");
 // const inputQtySwap = document.getElementById("inputQtySwap");
 
+// DOM elements that are being targeted often
+const allTextDetails = document.querySelectorAll(".product__details > .text--details");
+const allTextTitles = document.querySelectorAll(".product__details > h3");
+const allProductsDetails = document.querySelectorAll(".product__details");
+const oldPriceText = document.getElementById("sec__price--old");
+
+// Sections
+const sectionOrder = document.getElementById("sectionOrder");
+const sectionShipping = document.getElementById("sectionShipping");
+const sectionPersonal = document.getElementById("sectionPersonal");
+const sectionPayment = document.getElementById("sectionPayment");
+
 
 // Data storage
 const prices = {
@@ -39,6 +54,8 @@ let User = {
     powerBanks: 0,
     shippingPrice: 0,
     paymentChoice: "",
+    totalSwapPrice: 0,
+    totalBankPrice: 0,
     totalPrice: 0,
     userData: {
         fullName: "",
@@ -58,48 +75,113 @@ document.addEventListener("DOMloaded", init());
 function init() {
     console.log("Initializing....");
     addEventListeners();
-    updatePrices();
+    initPrices();
+
+    hideElement(sectionShipping);
+    hideElement(sectionPersonal);
+    hideElement(sectionPayment);
+    hideElement(btnFinalConfirm);
+
 
 }
 
 function addEventListeners() {
     console.log("... adding event listeners");
-
-    btnOrder.addEventListener("click", () => {
-        showElement(btnEditOrder);
-        hideElement(btnOrder);
-
-        User.swapServices = document.getElementById("inputQtySwap").value;
-        User.powerBanks = document.getElementById("inputQtyBattery").value;
-
-
-        console.log(User);
-
-    });
+    // confirming section btns
+    btnOrder.addEventListener("click", () => confirmOrder());
     btnShipping.addEventListener("click", () => {});
     btnPersonal.addEventListener("click", () => {});
     btnPayment.addEventListener("click", () => {});
-
+    // edit section btns
     btnEditOrder.addEventListener("click", () => {
         showElement(btnOrder);
         hideElement(btnEditOrder);
-    });
+        allTextDetails.forEach(elem => showElement(elem));
+        allTextTitles.forEach(elem => elem.style.fontWeight = "900");
+        allTextTitles.forEach(elem => elem.style.fontSize = "1em");
+        allTextTitles.forEach(elem => elem.style.width = "auto");
+        allProductsDetails.forEach(elem => elem.style.display = "inline");
+        showElement(oldPriceText);
+        showElement(inputQtySwap);
+        showElement(inputQtyBattery);
 
+        document.getElementById("step1-5").style.fill = "white";
+        document.getElementById("step2").style.fill = "white";
+        document.getElementById("active").style.transform = "translateX(0%)";
+        
+
+    });
     btnEditShipping.addEventListener("click", () => { });
     btnEditPersonal.addEventListener("click", () => { });
     btnEditPayment.addEventListener("click", () => { });
-
-    inputQtySwap.addEventListener("change", (e) => {
-        User.swapServices = parseFloat(inputQtySwap.value);
-        User.shippingPrice = prices.swapNewPrice * parseFloat(inputQtySwap.value);
-        console.log(User);
-    });
-    inputQtyBattery.addEventListener("change", (e) => {
-        User.powerBanks = parseFloat(inputQtyBattery.value);
-        User.shippingPrice = prices.swapNewPrice * parseFloat(inputQtySwap.value);
-        console.log(User);
-    });
+    // listen to input changes
+    inputQtySwap.addEventListener("change", () => updateOrderInputs());
+    inputQtyBattery.addEventListener("change", () => updateOrderInputs());
 }
+
+function updateOrderInputs() {
+    User.swapServices = parseFloat(inputQtySwap.value);
+    User.powerBanks = parseFloat(document.getElementById("inputQtyBattery").value);
+    User.totalSwapPrice = User.swapServices * prices.swapNewPrice;
+    User.totalBankPrice = User.powerBanks * prices.powerBankPrice;
+    let oldServicePrice = prices.swapOldPrice * User.swapServices;
+    updatePrices(User.totalSwapPrice, oldServicePrice, User.totalBankPrice);
+
+    // User.shippingPrice = prices.swapNewPrice * parseFloat(inputQtySwap.value);
+
+
+
+}
+
+
+
+function confirmOrder() {
+    showElement(btnEditOrder);
+    hideElement(btnOrder);
+
+    allTextDetails.forEach(elem => hideElement(elem));
+    allTextTitles.forEach(elem => elem.style.fontWeight = "700");
+    allTextTitles.forEach(elem => elem.style.fontSize = "0.8em");
+    allTextTitles.forEach(elem => elem.style.width = "40vw");
+    allProductsDetails.forEach(elem => elem.style.display = "flex");
+    allProductsDetails.forEach(elem => (elem.style.justifyContent = "space-between"));
+    hideElement(oldPriceText);
+    hideElement(inputQtySwap);
+    hideElement(inputQtyBattery);
+    
+    document.getElementById("step1-5").style.fill = "#a6cc45";
+    document.getElementById("step2").style.fill = "#a6cc45";
+    document.getElementById("active").style.transform = "translateX(45%)";
+
+    showElement(sectionShipping);
+
+    console.log(User);
+
+}
+
+
+//         shippingPrice: 0,
+//             paymentChoice: "",
+//                         totalPrice: 0,
+
+
+
+
+
+
+function updatePrices(servicePrice, oldServicePrice, bankPrice) {
+    document.getElementById("sec__price--new").innerHTML =
+      servicePrice + " DKK";
+    document.getElementById("sec__price--old").innerHTML =
+      oldServicePrice + " DKK";
+    document.getElementById("sec__price").innerHTML = bankPrice + " DKK";
+
+    console.log(servicePrice, oldServicePrice, bankPrice);
+    
+}
+
+
+
 
 
 
@@ -110,9 +192,9 @@ function hideElement(elem) {
     elem.classList.add("hide");
 }
 function showElement(elem) {
-  elem.classList.remove("hide");
+    elem.classList.remove("hide");
 }
-function updatePrices() {
+function initPrices() {
     document.getElementById("sec__price--new").innerHTML = prices.swapNewPrice + " DKK";
     document.getElementById("sec__price--old").innerHTML = prices.swapOldPrice + " DKK";
     document.getElementById("sec__price").innerHTML = prices.powerBankPrice + " DKK";
